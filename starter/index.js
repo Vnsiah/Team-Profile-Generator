e01b9a9dcff8a7c84db3328f41f45bcdfe58dbae
Vida Nsiah <vidansiah@Vidas-MacBook-Air.local>
2023-02-24 12:12:53 +0000

    creating inquirer

--- starter/index.js
+++ starter/index.js
@@ -3,13 +3,25 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const Employee = require("./lib/Employee");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+let team = [];
+
+startProgram()
+async function startProgram(){
+
+    team.push(new Employee("vida", 1, "test@test.com"))
+
+    let htmlDoc = render(team)
+
+    await fs.writeFile(outputPath, htmlDoc);
+}
