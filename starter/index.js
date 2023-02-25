f266b146282b29edfa41578e2acfdb63b8311c9b
Vida Nsiah <vidansiah@Vidas-MacBook-Air.local>
2023-02-25 08:39:48 +0000

    creating team html

--- starter/index.js
+++ starter/index.js
@@ -20,113 +20,17 @@ let team = [];
 startProgram()
 async function startProgram() {
 
-    questions()
+    team.push(new Manager("KOFI", 1234, "test@test.com", 10000))
+    team.push(new Engineer("YAW", 234, "rest@test.com", "@dash"))
+    team.push(new Intern("YAA", 2345, "Wrest@test.com", "Mayville"))
+
+    let htmlDoc = render(team)
+
+    await fs.writeFile(outputPath, htmlDoc);
+
+    // questions()
 
 
 
-}
-
-//setting Questions for team
-
-function questions() {
-    inquirer.prompt(
-        {
-            type: 'list',
-            name: 'employeeType',
-            message: "Which type of employee would you like to add to the team?",
-            choices: ['Manager', 'Engineer', 'Intern', 'I finished entering my team info']
-        })
-        .then(({ employeeType }) => {
-            if (employeeType === 'Manager') {
-                inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'name',
-                        message: "Please enter the manager's name",
-                        validate: nameInput => {
-                            if (nameInput) {
-                                return true;
-                            } else {
-                                console.log("Please enter the manager's name!");
-                                return false;
-                            }
-                        }
-                    },
-                    {
-                        type: 'number',
-                        name: 'id',
-                        message: "Please enter the manager's employee id",
-                        validate: idInput => {
-                            if (idInput) {
-                                return true;
-                            } else {
-                                console.log("Please enter a correct answer, the employee id should be a number!");
-                                return false;
-                            }
-                        }
-                    },
-                    {
-                        type: 'input',
-                        name: 'email',
-                        message: "Please enter the manager's email",
-                        validate: emailInput => {
-                            if (emailInput) {
-                                return true;
-                            } else {
-                                console.log("Please enter the correct manager's email!");
-                                return false;
-                            }
-                        }
-                    },
-                    {
-                        type: 'number',
-                        name: 'officeNumber',
-                        message: "Please enter the manager's office number",
-                        validate: officeNumberInput => {
-                            if (officeNumberInput) {
-                                return true;
-                            } else {
-                                console.log("Please enter a correct answer, the office number should be a number!");
-                                return false;
-                            }
-                        }
-                    },
-                ])
-
-                    // Pushes Manager data into teamArray
-                    .then((templateData) => {
-                        const newManager = new Manager(templateData.name, templateData.id, templateData.email, templateData.officeNumber)
-                        team.push(newManager);
-                        // Sends user back to menu
-                        questions();
-                    });
-            }
-            if (employeeType === 'Engineer') {
-
-
-               // prompt for engineer question 
-            }
-
-
-            if (employeeType === 'Intern') {
-
-
-                // prompt for intern question 
-             }
-
-
-             if (employeeType === 'I finished entering my team info') {
-
-                let htmlDoc = render(team)
-             fs.writeFile(outputPath, htmlDoc);
-
-
-
-                // prompt for engineer question 
-             }
-
-        
-
-        })
 }
 
