7f31da2855d802a9a784b4da18485773c0a8a6c9
Vida Nsiah <vidansiah@Vidas-MacBook-Air.local>
2023-02-26 12:16:43 +0000

    adding interns

--- starter/index.js
+++ starter/index.js
@@ -33,173 +33,235 @@ async function startProgram() {
 
 
 
-//setting Questions for team
+    //setting Questions for Manager
 
-function questions() {
-    inquirer.prompt(
-        {
-            type: 'list',
-            name: 'employeeType',
-            message: "Which type of employee would you like to add to the team?",
-            choices: ['Manager', 'Engineer', 'Intern', 'I finished entering my team info']
-        })
-        .then(({ employeeType }) => {
-            if (employeeType === 'Manager') {
-                inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'name',
-                        message: "Please enter the manager's name",
-                        validate: nameInput => {
-                            if (nameInput) {
-                                return true;
-                            } else {
-                                console.log("Please enter the manager's name!");
-                                return false;
+    function questions() {
+        inquirer.prompt(
+            {
+                type: 'list',
+                name: 'employeeType',
+                message: "Which type of employee would you like to add to the team?",
+                choices: ['Manager', 'Engineer', 'Intern', 'I finished entering my team info']
+            })
+            .then(({ employeeType }) => {
+                if (employeeType === 'Manager') {
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'name',
+                            message: "Please enter the manager's name",
+                            validate: nameInput => {
+                                if (nameInput) {
+                                    return true;
+                                } else {
+                                    console.log("Please enter the manager's name!");
+                                    return false;
+                                }
                             }
-                        }
-                    },
-                    {
-                        type: 'number',
-                        name: 'id',
-                        message: "Please enter the manager's employee id",
-                        validate: idInput => {
-                            if (idInput) {
-                                return true;
-                            } else {
-                                console.log("Please enter a correct answer, the employee id should be a number!");
-                                return false;
+                        },
+                        {
+                            type: 'number',
+                            name: 'id',
+                            message: "Please enter the manager's employee id",
+                            validate: idInput => {
+                                if (idInput) {
+                                    return true;
+                                } else {
+                                    console.log("Please enter a correct answer, the employee id should be a number!");
+                                    return false;
+                                }
                             }
-                        }
-                    },
-                    {
-                        type: 'input',
-                        name: 'email',
-                        message: "Please enter the manager's email",
-                        validate: emailInput => {
-                            if (emailInput) {
-                                return true;
-                            } else {
-                                console.log("Please enter the correct manager's email!");
-                                return false;
+                        },
+                        {
+                            type: 'input',
+                            name: 'email',
+                            message: "Please enter the manager's email",
+                            validate: emailInput => {
+                                if (emailInput) {
+                                    return true;
+                                } else {
+                                    console.log("Please enter the correct manager's email!");
+                                    return false;
+                                }
                             }
-                        }
-                    },
-                    {
-                        type: 'number',
-                        name: 'officeNumber',
-                        message: "Please enter the manager's office number",
-                        validate: officeNumberInput => {
-                            if (officeNumberInput) {
-                                return true;
-                            } else {
-                                console.log("Please enter a correct answer, the office number should be a number!");
-                                return false;
+                        },
+                        {
+                            type: 'number',
+                            name: 'officeNumber',
+                            message: "Please enter the manager's office number",
+                            validate: officeNumberInput => {
+                                if (officeNumberInput) {
+                                    return true;
+                                } else {
+                                    console.log("Please enter a correct answer, the office number should be a number!");
+                                    return false;
+                                }
                             }
-                        }
-                    },
-                ])
+                        },
+                    ])
 
-                    // this should Push Manager data into team
-                    .then((templateData) => {
-                        const newManager = new Manager(templateData.name, templateData.id, templateData.email, templateData.officeNumber)
-                        team.push(newManager);
-                        // Sends user back to menu
+                        // this should Push Manager data into team
+                        .then((templateData) => {
+                            const newManager = new Manager(templateData.name, templateData.id, templateData.email, templateData.officeNumber)
+                            team.push(newManager);
+                            // then sends the user back to menu
+                            questions();
+                        });
+                }
+                if (employeeType === 'Engineer') {
+
+                    // prompt for engineer question 
+
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'name',
+                            message: "Please enter the engineer's name",
+                            validate: nameInput => {
+                                if (nameInput) {
+                                    return true;
+                                } else {
+                                    console.log("Please enter the engineer's name!");
+                                    return false;
+                                }
+                            }
+                        },
+                        {
+                            type: 'number',
+                            name: 'id',
+                            message: "Please enter the engineer's employee id",
+                            validate: idInput => {
+                                if (idInput) {
+                                    return true;
+                                } else {
+                                    console.log("Please enter a correct answer, the employee id should be a number!");
+                                    return false;
+                                }
+                            }
+                        },
+                        {
+                            type: 'input',
+                            name: 'email',
+                            message: "Please enter the engineer's email",
+                            validate: emailInput => {
+                                if (emailInput) {
+                                    return true;
+                                } else {
+                                    console.log("Please enter the correct engineer's email!");
+                                    return false;
+                                }
+                            }
+                        },
+                        {
+                            type: 'input',
+                            name: 'github',
+                            message: "Please enter the engineer's github username",
+                            validate: githubInput => {
+                                if (githubInput) {
+                                    return true;
+                                } else {
+                                    console.log("Please enter the correct engineer's github username!");
+                                    return false;
+                                }
+                            }
+                        }
+
+                        // this should Push Engineer data into team
+                    ]).then(templateData => {
+                        const newEngineer = new Engineer(templateData.name, templateData.id, templateData.email, templateData.github);
+                        team.push(newEngineer);
+                        // then sends the user back to menu
                         questions();
                     });
-            }
-            if (employeeType === 'Engineer') {
-
-                // prompt for engineer question 
-
-                inquirer.prompt([
-                    {
-                        type: 'input',
-                        name: 'name',
-                        message: "Please enter the engineer's name",
-                        validate: nameInput => {
-                            if (nameInput) {
-                                return true;
-                            } else {
-                                console.log("Please enter the engineer's name!");
-                                return false;
-                            }
-                        }
-                    },
-                    {
-                        type: 'number',
-                        name: 'id',
-                        message: "Please enter the engineer's employee id",
-                        validate: idInput => {
-                            if (idInput) {
-                                return true;
-                            } else {
-                                console.log("Please enter a correct answer, the employee id should be a number!");
-                                return false;
-                            }
-                        }
-                    },
-                    {
-                        type: 'input',
-                        name: 'email',
-                        message: "Please enter the engineer's email",
-                        validate: emailInput => {
-                            if (emailInput) {
-                                return true;
-                            } else {
-                                console.log("Please enter the correct engineer's email!");
-                                return false;
-                            }
-                        }
-                    },
-                    {
-                        type: 'input',
-                        name: 'github',
-                        message: "Please enter the engineer's github username",
-                        validate: githubInput => {
-                            if (githubInput) {
-                                return true;
-                            } else {
-                                console.log("Please enter the correct engineer's github username!");
-                                return false;
-                            }
-                        }
-                    }
-
-                    // this should Push Engineer data into team
-                ]).then(templateData => {
-                    const newEngineer = new Engineer(templateData.name, templateData.id, templateData.email, templateData.github);
-                    team.push(newEngineer);
-                    // Sends user back to menu
-                    questions();
-                });
-            }
+                }
 
 
-            if (employeeType === 'Intern') {
-
+                if (employeeType === 'Intern') {
                 
+            // prompt for engineer question 
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'name',
+                            message: "Please enter the intern's name",
+                            validate: nameInput => {
+                                if (nameInput) {
+                                    return true;
+                                } else {
+                                    console.log("Please enter the intern's name!");
+                                    return false;
+                                }
+                            }
+                        },
+                        {
+                            type: 'number',
+                            name: 'id',
+                            message: "Please enter the intern's employee id",
+                            validate: idInput => {
+                                if (idInput) {
+                                    return true;
+                                } else {
+                                    console.log("Please enter a correct answer, the employee id should be a number!");
+                                    return false;
+                                }
+                            }
+                        },
+                        {
+                            type: 'input',
+                            name: 'email',
+                            message: "Please enter the intern's email",
+                            validate: emailInput => {
+                                if (emailInput) {
+                                    return true;
+                                } else {
+                                    console.log("Please enter the correct intern's email!");
+                                    return false;
+                                }
+                            }
+                        },
+                        {
+                            type: 'input',
+                            name: 'school',
+                            message: "Please enter the intern's school name",
+                            validate: schoolInput => {
+                                if (schoolInput) {
+                                    return true;
+                                } else {
+                                    console.log("Please enter the correct intern school's name!");
+                                    return false;
+                                }
+                            }
+                        }
+
+                        // this should Push Intern data into team
+                    ]).then(templateData => {
+                        const newIntern = new Intern(templateData.name, templateData.id, templateData.email, templateData.school);
+                        team.push(newIntern);
+                        // then Sends the user back to menu
+                        questions();
+                    });
 
 
-               
-            }
+                }
 
 
-            if (employeeType === 'I finished entering my team info') {
+                if (employeeType === 'I finished entering my team info') {
 
-                let htmlDoc = render(team)
-                fs.writeFile('./starter/output/team.html', htmlDoc);
-
-
-
-               
-            }
+                    let htmlDoc = render(team)
+                    fs.writeFile(outputPath, htmlDoc);
 
 
 
-        })
+
+                }
+
+
+
+            })
+    }
+
 }
 
-}
+// questions();
 
 
